f9c2cd364f9cd5be9578e51a1701054dec87184c
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-12-17 21:56:23 +0000

    -h

---
+++ Level04/ft_split.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+static void	ft_allocate(char **tab, char const *s, char sep)
+{
+	char		**tab_p;
+	char const	*temp;
+
+	temp = s;
+	tab_p = tab;
+	while (*temp)
+	{
+		while (*s == sep)
+			++s;
+		temp = s;
+		while (*temp && *temp != sep)
+			++temp;
+		if (*temp == sep || temp > s)
+		{
+			*tab_p = ft_substr(s, 0, temp - s);
+			s = temp;
+			++tab_p;
+		}
+	}
+	*tab_p = NULL;
+}
+
+static int	ft_count_words(char const *s, char sep)
+{
+	int	word_count;
+
+	word_count = 0;
+	while (*s)
+	{
+		while (*s == sep)
+			++s;
+		if (*s)
+			++word_count;
+		while (*s && *s != sep)
+			++s;
+	}
+	return (word_count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**new;
+	int		size;
+
+	if (!s)
+		return (NULL);
+	size = ft_count_words(s, c);
+	new = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!new)
+		return (NULL);
+	ft_allocate(new, s, c);
+	return (new);
+}
+
+// Assignment name  : ft_split
+// Expected files   : ft_split.c
+// Allowed functions: malloc
+// --------------------------------------------------------------------------------
+
+// Write a function that takes a string, splits it into words, and returns them as
+// a NULL-terminated array of strings.
+
+// A "word" is defined as a part of a string delimited either by spaces/tabs/new
+// lines, or by the start/end of the string.
+
+// Your function must be declared as follows:
+
+// char    **ft_split(char *str);
