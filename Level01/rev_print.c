58659e36cb1aaae74e3adc84067a674380ef0da4
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-12-21 20:15:11 +0000

    -h

--- Level01/rev_print.c
+++ Level01/rev_print.c
@@ -12,19 +12,21 @@
 
 #include <unistd.h>
 
+void	rev_print(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	while (--i >= 0)
+		write (1, &str[i], 1);
+}
+
 int	main(int argc, char *argv[])
 {
-	int	i;
-
-	i = 0;
 	if (argc == 2)
-	{
-		while (argv[1][i])
-			i++;
-		while (i)
-			write(1, &argv[1][--i], 1);
-	}
-	write(1, "\n", 1);
+		rev_print(argv[1]);
+	write (1, "\n", 1);
 	return (0);
 }
 
