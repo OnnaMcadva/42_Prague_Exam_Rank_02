2d51134ed1d17592a5b036736f90637b070cf35a
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-12-23 00:13:46 +0100

    Update ft_split.c

--- Level04/ft_split.c
+++ Level04/ft_split.c
@@ -1,61 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: annavm <annavm@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 23:12:21 by annavm            #+#    #+#             */
+/*   Updated: 2023/12/23 00:12:33 by annavm           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
-#include <stddef.h>
+# define WD_NUM 1000
+# define WD_LEN 1000
 
-static void	ft_allocate(char **tab, char const *s, char sep)
+char	**ft_split(char *str)
 {
-	char		**tab_p;
-	char const	*temp;
+    int i = 0;
+    int j = 0;
+    int k;
+    char **tab;
 
-	temp = s;
-	tab_p = tab;
-	while (*temp)
-	{
-		while (*s == sep)
-			++s;
-		temp = s;
-		while (*temp && *temp != sep)
-			++temp;
-		if (*temp == sep || temp > s)
-		{
-			*tab_p = ft_substr(s, 0, temp - s);
-			s = temp;
-			++tab_p;
-		}
-	}
-	*tab_p = NULL;
+    tab = (char**)malloc(sizeof(**tab)*WD_NUM);
+    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+        i++;
+    while (str[i])
+    {
+        if (str[i] > 32)
+        {
+            k = 0;
+            tab[j] = (char*)malloc(sizeof(char)*WD_LEN);
+            while (str[i] > 32)
+            {
+                tab[j][k] = str[i];
+                k++;
+                i++;
+            }
+            tab[j][k] = '\0';
+            j++;
+        }
+        else
+            i++;
+    }
+    tab[j] = 0;
+    return (tab);
 }
 
-static int	ft_count_words(char const *s, char sep)
-{
-	int	word_count;
+/*
+#include <stdio.h>
 
-	word_count = 0;
-	while (*s)
-	{
-		while (*s == sep)
-			++s;
-		if (*s)
-			++word_count;
-		while (*s && *s != sep)
-			++s;
-	}
-	return (word_count);
+int main() {
+    char str[] = "что за херня xnj pf [thyz]   -ft_split";
+    char **result = ft_split(str);
+
+    // Вывод содержимого массива result
+    int i = 0;
+    while (result[i] != 0) {
+        printf("Word %d: %s\n", i + 1, result[i]);
+        i++;
+    }
+
+    // Освобождение выделенной памяти
+    i = 0;
+    while (result[i] != 0) {
+        free(result[i]);
+        i++;
+    }
+    free(result);
+
+    return 0;
 }
-
-char	**ft_split(char const *s, char c)
-{
-	char	**new;
-	int		size;
-
-	if (!s)
-		return (NULL);
-	size = ft_count_words(s, c);
-	new = (char **)malloc(sizeof(char *) * (size + 1));
-	if (!new)
-		return (NULL);
-	ft_allocate(new, s, c);
-	return (new);
-}
+*/
 
 // Assignment name  : ft_split
 // Expected files   : ft_split.c
