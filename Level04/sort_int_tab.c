885eca26bbefa13aa3ee055fc486ad1f6eeb44c4
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-12-26 23:20:17 +0100

    Update sort_int_tab.c

--- Level04/sort_int_tab.c
+++ Level04/sort_int_tab.c
@@ -1,4 +1,22 @@
 
+void sort_int_tab(int *tab, unsigned int size)
+{
+    unsigned int i = 0;
+    int temp;
+
+    while (i < (size - 1))
+    {
+        if (tab[i] > tab[i + 1])
+        {
+            temp = tab[i];
+            tab[i] = tab[i + 1];
+            tab[i + 1] = temp;
+            i = 0; // Обнуление счетчика для нового прохода
+        }
+        else
+            i++;
+    }
+}
 
 
 // Assignment name  : sort_int_tab
