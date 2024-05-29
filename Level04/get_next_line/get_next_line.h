f5775d6dc79e1d6cdfb198d40a191cf22e739634
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2024-05-29 13:02:42 +0200

    Update get_next_line.h

--- Level04/get_next_line/get_next_line.h
+++ Level04/get_next_line/get_next_line.h
@@ -10,16 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+// #ifndef GET_NEXT_LINE_H
+// # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdlib.h>
+// # include <unistd.h>
+// # include <stdlib.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+// #ifndef BUFFER_SIZE
+// # define BUFFER_SIZE 42
+// #endif
 
-char	*get_next_line(int fd);
+// char	*get_next_line(int fd);
 
-#endif
+// #endif
