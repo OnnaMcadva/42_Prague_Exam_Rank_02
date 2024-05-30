c5263fe631fbb43d7bf164bcbdaea77331b020ef
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2024-05-30 14:59:51 +0200

    Update ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,38 +6,67 @@
 /*   By: anmakaro <anmakaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 14:05:32 by anmakaro          #+#    #+#             */
-/*   Updated: 2023/12/10 14:05:33 by anmakaro         ###   ########.fr       */
+/*   Updated: 2024/05/30 14:05:33 by anmakaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
 
-void	put_string(char *string, int *length)
+void put_string(char *str, int *len)
 {
-	if (!string)
-		string = "(null)";
-	while (*string)
-		*length += write(1, string++, 1);
+    if (!str)
+        str = "(null)";
+    while (*str)
+        *len += write(1, str++, 1);
 }
 
-void	put_digit(long long int number, int base, int *length)
+void put_digit(long long int num, int base, int *len)
 {
-	char	*hexadecimal = "0123456789abcdef";
+    char *hex = "0123456789abcdef";
 
-	if (number < 0)
-	{
-		number *= -1;
-		*length += write(1, "-", 1);
-	}
-	if (number >= base)
-		put_digit((number / base), base, length);
-	*length += write(1, &hexadecimal[number % base], 1);
+    if (num < 0)
+    {
+        num *= -1;
+        *len += write(1, "-", 1);
+    }
+    if (num >= base)
+        put_digit((num / base), base, len);
+    *len += write (1, &hex[num % base], 1);
 }
 
-int	ft_printf(const char *format, ...)
+int is_spec(char c)
 {
-	int length = 0;
+    if ((c == 's') || (c == 'd') || (c == 'x'))
+        return (1);
+    else
+        return (0);
+}
 
-	va_list	pointer;
-	va_start(pointer, format);
+int	ft_printf(const char *str, ...)
+{
+    int len = 0;
+
+    va_list args;
+    va_start(args, str);
+
+    while (*str)
+    {
+        if ((*str == '%') && is_spec(*(str + 1)))
+        {
+            str++;
+            if (*str == 's')
+                put_string(va_arg(args, char *), &len);
+            else if (*str == 'd')
+                put_digit((long long int)va_arg(args, int), 10, &len);
+            else if (*str == 'x')
+                put_digit((long long int)va_arg(args, unsigned int), 16, &len);
+        }
+        else
+            len += write (1, str, 1);
+        str++;
+    }
+    return (va_end(args), len);
+}
